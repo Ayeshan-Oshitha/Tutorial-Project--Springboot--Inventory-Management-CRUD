cc7b41f3bc611c89549899197a44bd65651848f8
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-08-30 23:18:21 +0530

    2. Created basic structure

---
+++ InventoryManagementApp/src/test/java/com/ayeshan/InventoryManagementApp/InventoryManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayeshan.InventoryManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
