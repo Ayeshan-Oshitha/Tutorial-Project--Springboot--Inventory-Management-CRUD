5e154618a23c1a0f2d889e7db53bab8c60307dd9
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-08-31 14:24:51 +0530

    12. Create Products (Create option) - Save the data

--- InventoryManagementApp/src/main/java/com/ayeshan/InventoryManagementApp/controllers/ProductsController.java
+++ InventoryManagementApp/src/main/java/com/ayeshan/InventoryManagementApp/controllers/ProductsController.java
@@ -4,13 +4,24 @@ import com.ayeshan.InventoryManagementApp.models.Product;
 import com.ayeshan.InventoryManagementApp.models.ProductDto;
 import com.ayeshan.InventoryManagementApp.services.ProductRepository;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -34,5 +45,50 @@ public class ProductsController {
         return "products/CreateProduct";
     }
 
+    @PostMapping("/create")
+    public String createProduct(
+            @Valid @ModelAttribute ProductDto productDto,
+            BindingResult result
+    ) {
+        if (productDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("productDto", "imageFile", "The image file is required"));
+        }
+        if (result.hasErrors()){
+            return "products/CreateProduct";
+        }
+
+        MultipartFile image = productDto.getImageFile();
+        Date createAt = new Date();
+        String storageFileName = createAt.getTime() + "_" + image.getOriginalFilename();
+
+        try {
+            String uploadDir = "InventoryManagementApp/src/main/resources/static/images/";
+            Path uploadpath = Paths.get(uploadDir);
+
+            if (!Files.exists(uploadpath)) {
+                Files.createDirectories(uploadpath);
+            }
+
+            try (InputStream inputStream = image.getInputStream()) {
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        }catch (Exception ex){
+                System.out.println("Exception" + ex.getMessage());
+        }
+
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setBrand(productDto.getBrand());
+        product.setCategory(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        product.setDescription(productDto.getDescription());
+        product.setCreatedAt(createAt);
+        product.setImageFileName(storageFileName);
+
+        repo.save(product);
+
+        return "redirect:/products";
+    }
 
 }
