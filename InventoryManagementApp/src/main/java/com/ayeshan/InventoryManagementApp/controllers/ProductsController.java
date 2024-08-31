6dfaea8a6223db5aaf7ec653df59302d5beb9631
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-08-31 12:50:08 +0530

    9. Show the produce list ( Get All )

--- InventoryManagementApp/src/main/java/com/ayeshan/InventoryManagementApp/controllers/ProductsController.java
+++ InventoryManagementApp/src/main/java/com/ayeshan/InventoryManagementApp/controllers/ProductsController.java
@@ -1,9 +1,16 @@
 package com.ayeshan.InventoryManagementApp.controllers;
 
+import com.ayeshan.InventoryManagementApp.models.Product;
 import com.ayeshan.InventoryManagementApp.services.ProductRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/products")
@@ -12,5 +19,11 @@ public class ProductsController {
     @Autowired
     private ProductRepository repo;
 
+    @GetMapping({"","/"})
+    public String showProductList(Model model){
+        List<Product> products = repo.findAll(); //Add this inside brackets to reverse the order of Id  =>  Sort.by(Sort.Direction.DESC,"id")
+        model.addAttribute("products",products);
+        return "products/index";
+    }
 
 }
